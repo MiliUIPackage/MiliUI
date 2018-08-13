0db7f2a19ec15f1404d09cc2ba781ea9e562b1e5
Mili Chang <michang@blizzard.com>
2018-08-14 03:51:36 +0800

    BT4預設樣式調整。

--- AddOns/Bartender4/Options/Presets.lua
+++ AddOns/Bartender4/Options/Presets.lua
@@ -35,7 +35,7 @@ local function BuildSingleProfile()
 	end
 
 	Bartender4.db.profile.blizzardVehicle = false
-	Bartender4.db.profile.outofrange = "hotkey"
+	Bartender4.db.profile.outofrange = "button"
 	Bartender4.db.profile.focuscastmodifier = false
 
 	config = Bartender4.db:GetNamespace("ActionBars").profile
@@ -87,7 +87,7 @@ local function BuildDoubleProfile()
 	end
 
 	Bartender4.db.profile.blizzardVehicle = true
-	Bartender4.db.profile.outofrange = "hotkey"
+	Bartender4.db.profile.outofrange = "button"
 	Bartender4.db.profile.focuscastmodifier = false
 
 	config = Bartender4.db:GetNamespace("ActionBars").profile
@@ -123,7 +123,7 @@ local function BuildDoubleProfile()
 	end
 
 	config = Bartender4.db:GetNamespace("BlizzardArt").profile
-	config.enabled = true
+	config.enabled = false
 	config.artLayout = "TWOBAR"
 	Bartender4:GetModule("BlizzardArt"):Enable()
 	SetBarLocation( config, "BOTTOM", -512, 47 )
@@ -183,7 +183,7 @@ local function BuildBlizzardProfile()
 	end
 
 	config = Bartender4.db:GetNamespace("BlizzardArt").profile
-	config.enabled = true
+	config.enabled = false
 	Bartender4:GetModule("BlizzardArt"):Enable()
 	SetBarLocation( config, "BOTTOM", -512, 47 )
 
