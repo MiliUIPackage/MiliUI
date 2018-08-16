d683dee2ce09aa4068130b6f66b4ddb85b996db6
Mili Chang <michang@blizzard.com>
2018-08-17 00:49:37 +0800

    Grid Click Sets 進遊戲不再出現提示文字。

--- AddOns/GridClickSets/GridClickSetsFrame.lua
+++ AddOns/GridClickSets/GridClickSetsFrame.lua
@@ -43,9 +43,9 @@ function GridClickSetsFrame_OnEvent(self, event, arg1)
     elseif(event=="VARIABLES_LOADED") then
         if not U1 then
             if(GetLocale()=="zhCN") then
-                DEFAULT_CHAT_FRAME:AddMessage("GridClickSets - |cffff3333爱不易|r (github.com/aby-ui)")
+                -- DEFAULT_CHAT_FRAME:AddMessage("GridClickSets - |cffff3333爱不易|r (github.com/aby-ui)")
             elseif(GetLocale()=="zhTW") then
-                DEFAULT_CHAT_FRAME:AddMessage("GridClickSets - |cff3399FF三月十二|r@聖光之願|cffFF00FF<冰封十字軍>|r.")
+                -- DEFAULT_CHAT_FRAME:AddMessage("GridClickSets - |cff3399FF三月十二|r@聖光之願|cffFF00FF<冰封十字軍>|r.")
             end
         else
             if _G.ClassMods and _G.ClassMods.Options.DB.clicktocast.enabled then
