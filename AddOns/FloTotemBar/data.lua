409d78cba431074a32d4da12760cd415d008480a
Mili Chang <michang@blizzard.com>
2018-08-16 22:55:31 +0800

    UI中文標題整理。

---
+++ AddOns/FloTotemBar/data.lua
@@ -0,0 +1,34 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this file,
+-- You can obtain one at http://mozilla.org/MPL/2.0/.
+
+FLO_TOTEM_SPELLS = {
+	["HUNTER"] = {
+		["TRAP"] = {
+			{ id = 187650, algo = 2 }, -- Freezing trap
+			{ id = 162488, algo = 2 }, -- Steel trap
+			{ id = 187698, algo = 3 }, -- Tar trap
+			{ id = 1543, duration = 20, algo = 4 }, -- Flare
+			{ id = 109248, duration = 10, algo = 4 }
+		}
+	},
+	["SHAMAN"] = {
+		["EARTH"] = {
+            { id = 2484, duration = 20 }, -- 8 Earthbind Totem
+			{ id = 5394, duration = 15, talented = 157153 }, -- 13 Healing Stream Totem / Cloudburst Totem
+			{ id = 210643, duration = 120, talented = 262395 }, -- 15 Totem Mastery
+			{ id = 192077, duration = 15 }, -- 30 Wind Rush Totem
+			{ id = 51485, duration = 20 }, -- 45 Earthgrab Totem
+			{ id = 192058, duration = 2 }, -- 45 Lightning Surge Totem
+			{ id = 196932, duration = 10 }, -- 45 Voodoo Totem
+			{ id = 61882, duration = 10 }, -- 52 Earthquake Totem
+			{ id = 108280, duration = 10 }, -- 54 Healing Tide Totem
+			{ id = 8143, duration = 10 }, -- 58 Tremor Totem
+			{ id = 98008, duration = 6 }, -- 62 Spirit Link Totem
+			{ id = 207399, duration = 30 }, -- 75 Ancestral Protection Totem
+			{ id = 198838, duration = 15 }, -- 75 Earthen Shield Totem
+			{ id = 192222, duration = 15 }, -- 100 Liquid Magma Totem
+		}
+	}
+};
+
