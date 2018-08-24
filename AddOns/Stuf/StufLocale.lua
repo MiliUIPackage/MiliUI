6d6315a789d05701d07bdd32775e1abbba3778a6
Mili Chang <michang@blizzard.com>
2018-08-25 01:47:20 +0800

    加入STUF翻譯檔案

---
+++ AddOns/Stuf/StufLocale.lua
@@ -0,0 +1,20 @@
+-- 2016/9/6 Triditional Chinese translated by 彩虹ui
+
+if ( GetLocale() == "zhTW" ) then
+StufLocalization = {
+
+["|cff00ff00Stuf|r: "] = "|cff00ff00Stuf頭像|r：",
+["Cannot load Stuf while in combat."] = "戰鬥中無法載入。",
+["Settings copied to this character."] = "已複製設定到這個角色。",
+["Stuf_Options is required to initialize variables."] = "需要載入 Stuf頭像-設定選項 模組。",
+["Stuf_Options not found."] = "無法找到 Stuf頭像-設定選項 模組。",
+["%s is using version %s."] = "%s 目前正在使用的版本是 %s。",
+["Humanoid"] = "人形生物",
+["Stuf"] = "Stuf頭像",
+["Inspect"] = "觀察",
+[" <Left-click> to inspect.\n"] = " <左鍵> 觀察\n",
+[" <Middle-click> to note target.\n"] = " <中鍵> 設定註記\n",
+[" <Right-click> to dressup."] = " <右鍵> 試衣間",
+
+}
+end
