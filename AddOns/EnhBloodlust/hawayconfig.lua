07819bc8d95e2a3f14a6b53d8e94319725060d25
Mili Chang <michang@blizzard.com>
2020-08-06 17:54:32 +0800

    嗜血音樂調整

--- AddOns/EnhBloodlust/hawayconfig.lua
+++ AddOns/EnhBloodlust/hawayconfig.lua
@@ -10,12 +10,13 @@ EnhBloodlustConfig = {};
 local config = EnhBloodlustConfig;
 
 -- Bloodlust, Heroism, Time Warp, Ancient Hysteria
-config.spells = {2825, 32182, 80353, 90355, 160452, 178207, 230935, 264667}
+config.spells = {2825, 32182, 80353, 90355, 160452, 178207, 230935, 264667, 256740, 292686}
 
 config.sound = {
-	-- 加入音樂檔案路徑，一行一首歌用逗號分隔，會隨機播放。
-	-- 最後一首歌 (最後一行) 的後面不要加逗號。
+	-- 加入音樂檔案路徑，一行一首歌，結尾加上逗號，會隨機播放。
+	-- 最後一首歌 (最後一行) 的結尾不要加逗號。
 	-- 每一行前面加上兩條橫線的歌曲不會播放。
+	-- "Interface\\AddOns\\EnhBloodlust\\音樂檔案名稱.副檔名",
 	"Interface\\AddOns\\EnhBloodlust\\Is_She_With_You.mp3",
 	"Interface\\AddOns\\EnhBloodlust\\bloodlust_mid.mp3"
 }
@@ -24,7 +25,7 @@ config.sound = {
 config.length = 40;
 
 -- 嗜血音樂所使用的聲音頻道，可以使用的值有主音量 "Master" 和法術音效 "SFX"。
-config.channel = "Master";
+config.channel = "SFX";
 
 --[[
 --Some other examples
