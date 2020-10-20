f81052a84a8fb04641e570107c01c3a6d88e912d
Mili Chang <michang@blizzard.com>
2020-10-21 00:24:42 +0800

    Bagnon 預設值調整。

--- AddOns/Bagnon/common/Wildpants/Wildpants.lua
+++ AddOns/Bagnon/common/Wildpants/Wildpants.lua
@@ -34,12 +34,12 @@ end
 
 local FrameDefaults = {
 	enabled = true,
-	money = true, broker = true,
+	money = true, broker = false,
 	bagToggle = true, sort = true, search = true, options = true,
 
 	strata = 'HIGH', alpha = 1,
 	scale = Addon.FrameScale or 1,
-	color = {0, 0, 0, 0.5},
+	color = {0.1254901960784314, 0.1254901960784314, 0.1254901960784314, 1},
 	x = 0, y = 0,
 
 	itemScale = Addon.ItemScale or 1,
@@ -61,10 +61,10 @@ local FrameDefaults = {
 local ProfileDefaults = {
 	inventory = SetDefaults({
 		reversedTabs = true,
-		borderColor = {1, 1, 1, 1},
+		borderColor = {0, 0, 0, 0.5},
 		point = 'BOTTOMRIGHT',
 		x = -50, y = 100,
-		columns = 8,
+		columns = 9,
 		width = 384,
 		height = 200,
 	}, FrameDefaults),
@@ -81,14 +81,14 @@ local ProfileDefaults = {
 	vault = SetDefaults({
 		borderColor = {1, 0, 0.98, 1},
 		point = 'LEFT',
-		columns = 10,
+		columns = 8,
 		x = 95
 	}, FrameDefaults),
 
 	guild = SetDefaults({
 		borderColor = {0, 1, 0, 1},
 		point = 'CENTER',
-		columns = 7,
+		columns = 20,
 	}, FrameDefaults)
 }
 
