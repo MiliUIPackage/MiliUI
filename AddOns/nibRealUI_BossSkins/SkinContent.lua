649a45295fd7a2b653d73d39727b55796d2ae241
Mili Chang <michang@blizzard.com>
2021-02-05 23:37:50 +0800

    修正BOSS技能提示樣式

---
+++ AddOns/nibRealUI_BossSkins/SkinContent.lua
@@ -0,0 +1,8 @@
+REALUIBOSSSKIN_BACKDROP_0116_1111={
+    bgFile=[[Interface\AddOns\nibRealUI_BossSkins\media\Plain]],
+    edgeFile=[[Interface\AddOns\nibRealUI_BossSkins\media\Plain]],
+    edgeSize=1,
+    tileSize=16,
+    insets={left=1,right=1,top=1,bottom=1},
+}
+REALUIBOSSKIN_BACKDROPCOLOR = CreateColor(0,0,0)
