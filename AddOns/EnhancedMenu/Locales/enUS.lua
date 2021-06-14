73400803c9320c9e435475579f66fb439c58777f
Mili <milichang0410@gmail.com>
2021-06-14 22:27:04 +0800

    [新增] EnhancedMenu

---
+++ AddOns/EnhancedMenu/Locales/enUS.lua
@@ -0,0 +1,15 @@
+select( 2, ... ).L = setmetatable({
+	["ENHANCED_MENU"] = "Enhanced Menu",
+	["GUILD_INVITE"] = "Guild Invite",
+	["COPY_NAME"] = "Copy Name",
+	["SEND_WHO"] = "Who",
+	["ARMORY_URL"] = "Armory",
+	["RAIDER_IO"] = "Raider.IO",
+}, {
+	__index = function(self, Key)
+		if (Key ~= nil) then
+			rawset(self, Key, Key)
+			return Key
+		end
+	end
+})
---
+++ AddOns/EnhancedMenu/Locales/zhCN.lua
@@ -0,0 +1,9 @@
+if not LOCALE_zhCN then return end
+
+local L = select( 2, ...).L
+
+L["ENHANCED_MENU"] = "增强菜单"
+L["GUILD_INVITE"] = "邀请入会"
+L["COPY_NAME"] = "复制名字"
+L["SEND_WHO"] = "查询"
+L["ARMORY_URL"] = "英雄榜"
