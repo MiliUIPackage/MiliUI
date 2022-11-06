93a48229583d7ff6e6b410ef32b0cdee44b629bf
Mili <milichang0410@gmail.com>
2022-11-06 15:44:53 +0800

    載入ui後出現訊息提示

---
+++ AddOns/MiliUI/core.lua
@@ -0,0 +1,5 @@
+if GetLocale() == "zhTW" then
+    C_Timer.After(1, function()
+        print("|cfffeff00歡迎使用|cffffd200米利UI套組|r，請隨時關注|cffffd200[奇樂-魔獸世界中文插件補給站](https://addons.miliui.com)|r以取得最新版本。|n若有任何問題歡迎在|cffffd200米利UI套組|r頁面下方留言討論。|r")
+    end)
+end
