2abc1ab4f6b5f02915912587bae44c4eeb63f6c3
Mili <milichang0410@gmail.com>
2022-11-07 00:47:13 +0800

    修正 shooter

--- AddOns/Shooter/Shooter.lua
+++ AddOns/Shooter/Shooter.lua
@@ -4,8 +4,10 @@ Shooter.frame = CreateFrame("Frame", "Shooter", UIParent);
 Shooter.frame:SetFrameStrata("BACKGROUND");
 
 Shooter.frame:SetScript("OnEvent",
-  function ()
-    C_Timer.After(1, Screenshot);
+  function()
+    C_Timer.After(1, function()
+      Screenshot()
+    end)
   end
 );
 
