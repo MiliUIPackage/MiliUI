337ccf5cf5084c725dc1905989059624287aecd3
Mili <milichang0410@gmail.com>
2023-01-20 04:22:11 +0800

    調整 TinyTooltip-Reforged 資料片id

--- AddOns/TinyTooltip-Reforged/ExpansionInfo.lua
+++ AddOns/TinyTooltip-Reforged/ExpansionInfo.lua
@@ -44,7 +44,7 @@ function AddExpansionInfo(tooltip, data)
         local itemID, bonusIDList, bonusIDCount = addon:ParseItemLink(itemLink, true)
         if (not expID) then return end
         if (expID > 0) then
-            tooltip:AddLine(format("|cffffdd22%s:|r |cff64cd3c%s|r (%d)", addon.L["Expansion"], _G['EXPANSION_NAME' .. expID], tonumber(expID)))
+            tooltip:AddLine(format("|cffffdd22%s:|r |cff64cd3c%s|r (%d)", addon.L["Expansion"], _G['EXPANSION_NAME' .. expID], tonumber(expID)+1))
         else
             tooltip:AddLine(format("|cffffdd22%s:|r |cff64cd3c%s|r", addon.L["Expansion"], _G['EXPANSION_NAME' .. expID]))
         end
