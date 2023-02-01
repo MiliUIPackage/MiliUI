18221dc79df166cbbcd17806f8bacf2651872305
Mili <milichang0410@gmail.com>
2023-02-02 02:55:22 +0800

    Bagnon 更新

--- AddOns/BagBrother/Libs/C_Everywhere/C_Everywhere.lua
+++ AddOns/BagBrother/Libs/C_Everywhere/C_Everywhere.lua
@@ -1,5 +1,5 @@
 --[[
-Copyright 2022-2022 João Cardoso
+Copyright 2022-2023 João Cardoso
 C_Everywhere is distributed under the terms of the GNU General Public License (Version 3).
 As a special exception, the copyright holders of this library give you permission to embed it
 with independent modules to produce an addon, regardless of the license terms of these
@@ -15,7 +15,7 @@ GNU General Public License for more details.
 This file is part of C_Everywhere.
 --]]
 
-local C = LibStub:NewLibrary('C_Everywhere', 2)
+local C = LibStub:NewLibrary('C_Everywhere', 4)
 if C then
   wipe(C)
 else
@@ -27,7 +27,7 @@ setmetatable(C, {__index = function(C, space)
   local container = {}
 
   setmetatable(container, {__index = function(container, k)
-    local f = container.rawfind(k)
+    local f = container.rawfind(k) or false
     container[k] = f
     return f
   end})
@@ -79,3 +79,24 @@ pack(C.Container, 'GetContainerItemQuestInfo', 'isQuestItem, questID, isActive')
 pack(C.CurrencyInfo, 'GetBackpackCurrencyInfo', 'name, quantity, iconFileID, currencyTypesID')
 pack(C.CurrencyInfo, 'GetCurrencyInfo', 'name, quantity, iconFileID, quantityEarnedThisWeek, maxWeeklyQuantity, maxQuantity, discovered, quality')
 pack(C.CurrencyInfo, 'GetCurrencyListInfo', 'name, isHeader, isHeaderExpanded, isTypeUnused, isShowInBackpack, quantity, iconFileID, maxQuantity, canEarnPerWeek, quantityEarnedThisWeek, discovered')
+
+if not C_TooltipInfo then
+  local tip = C_EverywhereTip or CreateFrame('GameTooltip', 'C_EverywhereTip', UIParent, 'GameTooltipTemplate')
+  local meta = getmetatable(tip).__index
+  tip:SetOwner(UIParent, 'ANCHOR_NONE')
+
+  C.TooltipInfo.hooksecurefunc = function(k, f) hooksecurefunc(meta, 'S' .. k:sub(2), f) end
+  C.TooltipInfo.locate = function() return meta end
+  C.TooltipInfo.rawfind = function(k)
+    local method = tip['S' .. k:sub(2)]
+    return function(...)
+      method(tip, ...)
+
+      local data = {lines={}}
+      for i = 1, tip:NumLines() do
+        data.lines[i] = {args = {nil, {stringVal = _G['C_EverywhereTipTextLeft' .. i]:GetText()}}}
+      end
+      return data
+    end
+  end
+end
