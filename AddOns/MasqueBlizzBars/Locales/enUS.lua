d91cc3b4493b769a55aec2a54858297bed31d820
Mili <milichang0410@gmail.com>
2023-04-11 01:41:55 +0800

    更新 MasqueBlizzBars

--- AddOns/MasqueBlizzBars/Locales/enUS.lua
+++ AddOns/MasqueBlizzBars/Locales/enUS.lua
@@ -3,54 +3,39 @@
 --
 -- Locales\enUS.lua -- enUS Localization File
 --
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by the
--- Free Software Foundation, either version 3 of the License, or (at your
--- option) any later version.
---
--- This program is distributed in the hope that it will be useful, but
--- WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
--- or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
--- more details.
---
--- You should have received a copy of the GNU General Public License along
--- with this program. If not, see <https://www.gnu.org/licenses/>.
+-- Use of this source code is governed by an MIT-style
+-- license that can be found in the LICENSE file or at
+-- https://opensource.org/licenses/MIT.
 --
 
 -- Please use CurseForge to submit localization content for another language:
 -- https://www.curseforge.com/wow/addons/masque-blizz-bars-revived/localization
 
-local Locale = GetLocale()
-if Locale ~= "enUS" then return end
+-- allow enUS to fill empty strings for other locales
+--local Locale = GetLocale()
+--if Locale ~= "enUS" then return end
 
 local _, Shared = ...
 local L = Shared.Locale
 
--- These are the names of the bars in game. Try to label them the way Blizzard refers to them.
-L["Action Bar 1"] = "Action Bar 1"
-L["Action Bar 2"] = "Action Bar 2"
-L["Action Bar 3"] = "Action Bar 3"
-L["Action Bar 4"] = "Action Bar 4"
-L["Action Bar 5"] = "Action Bar 5"
-L["Action Bar 6"] = "Action Bar 6"
-L["Action Bar 7"] = "Action Bar 7"
-L["Action Bar 8"] = "Action Bar 8"
-L["Pet Bar"] = "Pet Bar"
-L["Possess Bar"] = "Possess Bar"
-L["Stance Bar"] = "Stance Bar"
+-- Defaults for these are the keys themselves
+--L["Action Bar 1"] = "Action Bar 1"
+--L["Action Bar 2"] = "Action Bar 2"
+--L["Action Bar 3"] = "Action Bar 3"
+--L["Action Bar 4"] = "Action Bar 4"
+--L["Action Bar 5"] = "Action Bar 5"
+--L["Action Bar 6"] = "Action Bar 6"
+--L["Action Bar 7"] = "Action Bar 7"
+--L["Action Bar 8"] = "Action Bar 8"
+--L["Pet Bar"] = "Pet Bar"
+--L["Possess Bar"] = "Possess Bar"
+--L["Stance Bar"] = "Stance Bar"
+--L["Spell Flyouts"] = "Spell Flyouts"
+--L["Vehicle Bar"] = "Vehicle Bar"
+--L["Extra Ability Buttons"] = "Extra Ability Buttons"
+--L["Pet Battle Bar"] = "Pet Battle Bar"
 
--- This is only referred to in code. It refers to popup menus of abilities in the Spellbook that can be placed on action bars.
-L["Spell Flyouts"] = "Spell Flyouts"
-L["This group includes all flyouts shown anywhere in the game, such as Action Bars and the Spellbook."] = "This group includes all flyouts shown anywhere in the game, such as Action Bars and the Spellbook."
-
--- This is sometimes called the Override Bar and isn't really referred to in the game
-L["Vehicle Bar"] = "Vehicle Bar"
-L["This bar is shown when you enter a vehicle with abilities. The exit button is not currently able to be skinned."] = "This bar is shown when you enter a vehicle with abilities. The exit button is not currently able to be skinned."
-
--- This refers to the two types of large buttons.  In Edit Mode, it's referred to as Extra Abilities.
-L["Extra Ability Buttons"] = "Extra Ability Buttons"
-L["This group includes the Extra Action Button shown during encounters and quests, and all Zone Ability Buttons shown for location-based abilities.\n\nSome buttons have additional background images framing them, so square skins tend to work best."] = "This group includes the Extra Action Button shown during encounters and quests, and all Zone Ability Buttons shown for location-based abilities.\n\nSome buttons have additional background images framing them, so square skins tend to work best."
-
--- This is not really referred to in the game.
-L["Pet Battle Bar"] = "Pet Battle Bar"
-
+-- Using short keys for these long strings, so enUS needs to be defined as well
+L["NOTES_SPELL_FLYOUTS"] = "This group includes all flyouts shown anywhere in the game, such as Action Bars and the Spellbook."
+L["NOTES_VEHICLE_BAR"] = "This bar is shown when you enter a vehicle with abilities. The exit button is not currently able to be skinned."
+L["NOTES_EXTRA_ABILITY_BUTTONS"] = "This group includes the Extra Action Button shown during encounters and quests, and all Zone Ability Buttons shown for location-based abilities.\n\nSome buttons have additional background images framing them, so square skins tend to work best."
--- AddOns/MasqueBlizzBars/Locales/zhTW.lua
+++ AddOns/MasqueBlizzBars/Locales/zhTW.lua
@@ -1,20 +1,11 @@
 --
 -- Masque Blizzard Bars
 --
--- Locales\enUS.lua -- enUS Localization File
+-- Locales\zhTW.lua -- zhTW Localization File
 --
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by the
--- Free Software Foundation, either version 3 of the License, or (at your
--- option) any later version.
---
--- This program is distributed in the hope that it will be useful, but
--- WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
--- or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
--- more details.
---
--- You should have received a copy of the GNU General Public License along
--- with this program. If not, see <https://www.gnu.org/licenses/>.
+-- Use of this source code is governed by an MIT-style
+-- license that can be found in the LICENSE file or at
+-- https://opensource.org/licenses/MIT.
 --
 
 -- Please use CurseForge to submit localization content for another language:
@@ -35,23 +26,18 @@ L["Action Bar 6"] = "快捷列 6"
 L["Action Bar 7"] = "快捷列 7"
 L["Action Bar 8"] = "快捷列 8"
 L["Extra Ability Buttons"] = "額外技能按鈕"
-L["Notes"] = "請啟用 Masque 插件來更改遊戲內鍵快捷列的按鈕外觀。"
+L["NOTES_EXTRA_ABILITY_BUTTONS"] = [=[包含首領戰和任務的額外技能按鈕，以及特定區域才會出現的區域技能。
+
+有些技能會包圍著額外的背景圖案，所以方形的外觀似乎最適合。]=]
+L["NOTES_SPELL_FLYOUTS"] = "包含遊戲中所有會顯示彈出選單的技能群組，像是快捷列和法術書。"
+L["NOTES_VEHICLE_BAR"] = "上了有技能的載具時所顯示的快捷列，目前無法更改離開載具按鈕的外觀。"
 L["Pet Bar"] = "寵物列"
 L["Pet Battle Bar"] = "寵物對戰列"
 L["Possess Bar"] = "控制列"
 L["Spell Flyouts"] = "技能彈出選單"
 L["Stance Bar"] = "形態列"
-L["This bar is shown when you enter a vehicle with abilities. The exit button is not currently able to be skinned."] = "上了有技能的載具時所顯示的快捷列，目前無法更改離開載具按鈕的外觀。"
-L["This group includes all flyouts shown anywhere in the game, such as Action Bars and the Spellbook."] = "包含遊戲中所有會顯示彈出選單的技能群組，像是快捷列和法術書。"
-L[ [=[This group includes the Extra Action Button shown during encounters and quests, and all Zone Ability Buttons shown for location-based abilities.
-
-Some buttons have additional background images framing them, so square skins tend to work best.]=] ] = [=[包含首領戰和任務的額外技能按鈕，以及特定區域才會出現的區域技能。
-
-有些技能會包圍著額外的背景圖案，所以方形的外觀似乎最適合。]=]
 L["Vehicle Bar"] = "載具列"
 
 -- 自行加入
-L["Blizzard Action Bars"] = "遊戲內建快捷列"
-
-
-
+L["Blizzard Action Bars"] = "內建快捷列"
+L["Masque Blizzard Bars"] = "按鈕外觀-內建快捷列"
