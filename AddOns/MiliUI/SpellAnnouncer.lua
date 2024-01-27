7701cf6383d5753966f549ed24974b832948e9c8
Mili <milichang0410@gmail.com>
2024-01-28 05:53:07 +0800

    重構，解決團隊問題並加入製造靈魂之井與召喚餐點

--- AddOns/MiliUI/SpellAnnouncer.lua
+++ AddOns/MiliUI/SpellAnnouncer.lua
@@ -14,9 +14,13 @@ local portalSpellMap = {
     [424197] = "恆龍黎明",
 }
 
--- 術士傳送門
-local ritualOfSummoning = {
-    698,
+local needToHelp = {
+    698, -- 術士傳送門
+}
+
+local needToTake = {
+    29893, -- 製造靈魂之井
+    190336 -- 召喚餐點
 }
 
 -- 判斷表格是否包含特定值
@@ -34,49 +38,73 @@ function IsInCombat()
     return InCombatLockdown()
 end
 
+-- 增加計時器
+local spellCastTimer
+
+-- 施法通報
+local function HandleSpellMessage(unit, spellID, messagePrefix, detectAsPlayer)
+    local spellLink = GetSpellLink(spellID)
+    local name, realm = UnitName(unit)
+    local message = name .. "正在施放" .. spellLink .. messagePrefix
+
+    if (detectAsPlayer) then
+        if IsInGroup(LE_PARTY_CATEGORY_INSTANCE) or IsInRaid(LE_PARTY_CATEGORY_INSTANCE) then
+            SendChatMessage(message, "INSTANCE_CHAT")
+        elseif IsInRaid(LE_PARTY_CATEGORY_HOME) then
+            SendChatMessage(message, "RAID")
+        elseif IsInGroup(LE_PARTY_CATEGORY_HOME) then
+            SendChatMessage(message, "PARTY")
+        end
+    else
+        DEFAULT_CHAT_FRAME:AddMessage(string.rep("-", 50), 1, 0.64, 0)
+        print(' ')
+        print('|cfffdcb3b' .. message ..'|r')
+        print(' ')
+        DEFAULT_CHAT_FRAME:AddMessage(string.rep("-", 50), 1, 0.64, 0)
+    end
+
+end
+
+local function HandleSpellCast(unit, spellID, detectAsPlayer)
+    if portalSpellMap[spellID] then
+        HandleSpellMessage(unit, spellID, "傳送到" .. portalSpellMap[spellID] .. "！", detectAsPlayer)
+    end
+
+    if table.contains(needToHelp, spellID) then
+        HandleSpellMessage(unit, spellID, "，請協助點門。", detectAsPlayer)
+    end
+
+    if table.contains(needToTake, spellID) then
+        HandleSpellMessage(unit, spellID, "，請記得拿取。", detectAsPlayer)
+    end
+
+end
+
+-- 
+local detectAsPlayer = false
+
 -- 監聽事件
 SpellAnnouncer:SetScript("OnEvent", function(self, event, ...)
-    if event == "UNIT_SPELLCAST_START" or "UNIT_SPELLCAST_CHANNEL_START" then
-        local unit, spellGUID, spellID = ...
-        -- 檢查是否不在戰鬥中以及在隊伍中
-        -- 檢查施法者是否為玩家或隊伍成員
-        if not IsInCombat() and IsInGroup() and (unit == "player" or string.find(unit, "party")) then
-            -- 檢查是否施放的是傳送門法術
-            if portalSpellMap[spellID] then
-                local spellLink = GetSpellLink(spellID)
-                local name, realm = UnitName(unit)
-                local location = portalSpellMap[spellID]
-                local message = name .. "正在施放" .. spellLink .. "傳送到" .. location .. "！"
+    if (event == "UNIT_SPELLCAST_START") or (event == "UNIT_SPELLCAST_CHANNEL_START") then
+        if (not IsInCombat()) and (IsInGroup() or IsInRaid()) then
+            local unit, spellGUID, spellID = ...
+            if unit == "player" or string.find(unit, "party") then
                 if unit == "player" then
-                    SendChatMessage(message, "PARTY")
+                    detectAsPlayer = true
                 else
-                    DEFAULT_CHAT_FRAME:AddMessage(string.rep("-", 50), 1, 0.64, 0)
-                    print(' ')
-                    print('|cfffdcb3b' .. message ..'|r')
-                    print(' ')
-                    DEFAULT_CHAT_FRAME:AddMessage(string.rep("-", 50), 1, 0.64, 0)
+                    detectAsPlayer = false
                 end
-            end
 
-            if table.contains(ritualOfSummoning, spellID) then
-                local spellLink = GetSpellLink(spellID)
-                local name, realm = UnitName(unit)
-                local message = name .. "正在進行" .. spellLink .. "，請協助點門。"
-                if unit == "player" then
-                    if IsInGroup(LE_PARTY_CATEGORY_INSTANCE) or IsInRaid(LE_PARTY_CATEGORY_INSTANCE) then
-                        SendChatMessage(message, "INSTANCE_CHAT")
-                    elseif IsInRaid(LE_PARTY_CATEGORY_HOME) then
-                        SendChatMessage(message, "RAID")
-                    elseif IsInGroup(LE_PARTY_CATEGORY_HOME) then
-                        SendChatMessage(message, "PARTY")
-                    end
-                else
-                    DEFAULT_CHAT_FRAME:AddMessage(string.rep("-", 50), 1, 0.64, 0)
-                    print(' ')
-                    print('|cfffdcb3b' .. message ..'|r')
-                    print(' ')
-                    DEFAULT_CHAT_FRAME:AddMessage(string.rep("-", 50), 1, 0.64, 0)
+                -- 設置計時器，延遲處理
+                spellCastTimer = C_Timer.NewTimer(0.1, function()
+                    HandleSpellCast(unit, spellID, detectAsPlayer)
+                end)
+            elseif string.find(unit, "raid") then
+                -- 如果計時器存在，則取消計時器
+                if spellCastTimer then
+                    spellCastTimer:Cancel()
                 end
+                HandleSpellCast(unit, spellID, detectAsPlayer)
             end
         end
     end
