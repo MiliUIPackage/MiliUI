f8c865ce907182ad79eaff1f025c9cf5b7552374
Mili <milichang0410@gmail.com>
2024-07-25 11:58:59 +0800

    update TinyTooltip-Reforged

--- AddOns/TinyTooltip-Reforged/ExpansionInfo.lua
+++ AddOns/TinyTooltip-Reforged/ExpansionInfo.lua
@@ -40,11 +40,11 @@ function AddExpansionInfo(tooltip, data)
     if (data.type == Enum.TooltipDataType.Item) then
         local itemName, itemLink, itemID = TooltipUtil.GetDisplayedItem(tooltip)
         if (not itemLink) then return end
-        local expID = select(15, GetItemInfo(itemLink))
+        local expID = select(15, C_Item.GetItemInfo(itemLink))
         local itemID, bonusIDList, bonusIDCount = addon:ParseItemLink(itemLink, true)
         if (not expID) then return end
         if (expID > 0) then
-            tooltip:AddLine(format("|cffffdd22%s:|r |cff64cd3c%s|r (%d)", addon.L["Expansion"], _G['EXPANSION_NAME' .. expID], tonumber(expID)+1))
+            tooltip:AddLine(format("|cffffdd22%s:|r |cff64cd3c%s|r (%d)", addon.L["Expansion"], _G['EXPANSION_NAME' .. expID], tonumber(expID)))
         else
             tooltip:AddLine(format("|cffffdd22%s:|r |cff64cd3c%s|r", addon.L["Expansion"], _G['EXPANSION_NAME' .. expID]))
         end
