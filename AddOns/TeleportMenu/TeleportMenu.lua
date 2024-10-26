3b1990567d5be52573a5879048df109302842801
Mili <milichang0410@gmail.com>
2024-10-27 04:01:12 +0800

    fix TeleportMenu

--- AddOns/TeleportMenu/TeleportMenu.lua
+++ AddOns/TeleportMenu/TeleportMenu.lua
@@ -410,7 +410,7 @@ function tpm:CreateFlyout(flyoutData)
 
 	if db.buttonText == true and flyoutData.name then
 		button.text = button:CreateFontString(nil, "OVERLAY")
-		button.text:SetFont("Fonts\\FRIZQT__.TTF", 13, "OUTLINE")
+		button.text:SetFont("Fonts\\bHEI01B.TTF", 13, "OUTLINE")
 		button.text:SetPoint("BOTTOM", button, "BOTTOM", 0, 5)
 		button.text:SetText(flyoutData.name)
 		button.text:SetTextColor(1, 1, 1, 1)
@@ -503,7 +503,7 @@ function tpm:CreateFlyout(flyoutData)
 			local flyOutButton = createFlyOutButton(spellID, flyoutsCreated, totalKnownSpells)
 			if db.buttonText == true and flyname then
 				flyOutButton.text = flyOutButton:CreateFontString(nil, "OVERLAY")
-				flyOutButton.text:SetFont("Fonts\\FRIZQT__.TTF", 13, "OUTLINE")
+				flyOutButton.text:SetFont("Fonts\\bHEI01B.TTF", 13, "OUTLINE")
 				flyOutButton.text:SetPoint("BOTTOM", flyOutButton, "BOTTOM", 0, 5)
 				flyOutButton.text:SetText(flyname)
 				flyOutButton.text:SetTextColor(1, 1, 1, 1)
@@ -572,7 +572,7 @@ function tpm:CreateSeasonalTeleportFlyout()
 
 	if db.buttonText == true then
 		button.text = button:CreateFontString(nil, "OVERLAY")
-		button.text:SetFont("Fonts\\FRIZQT__.TTF", 13, "OUTLINE")
+		button.text:SetFont("Fonts\\bHEI01B.TTF", 13, "OUTLINE")
 		button.text:SetPoint("BOTTOM", button, "BOTTOM", 0, 5)
 		button.text:SetText(L["Season 1"])
 		button.text:SetTextColor(1, 1, 1, 1)
@@ -648,7 +648,7 @@ function tpm:CreateSeasonalTeleportFlyout()
 
 			if db.buttonText == true and flyname then
 				flyOutButton.text = flyOutButton:CreateFontString(nil, "OVERLAY")
-				flyOutButton.text:SetFont("Fonts\\FRIZQT__.TTF", 13, "OUTLINE")
+				flyOutButton.text:SetFont("Fonts\\bHEI01B.TTF", 13, "OUTLINE")
 				flyOutButton.text:SetPoint("BOTTOM", flyOutButton, "BOTTOM", 0, 5)
 				flyOutButton.text:SetText(flyname)
 				flyOutButton.text:SetTextColor(1, 1, 1, 1)
