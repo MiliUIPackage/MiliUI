ec5ec20096ae55d290edb3d8b999d50ae902d0d9
Mili <milichang0410@gmail.com>
2025-04-24 08:47:15 +0800

    update DBM

--- AddOns/DBM-Challenges/BfA/NzothVisionOrgrimmar.lua
+++ AddOns/DBM-Challenges/BfA/NzothVisionOrgrimmar.lua
@@ -1,13 +1,13 @@
 local mod	= DBM:NewMod("d1995", "DBM-Challenges", 2)--1993 Stormwind 1995 Org
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20211208050610")
+mod:SetRevision("20250411193139")
 
-mod:RegisterCombat("scenario", 2212)--2212, 2213 (org, stormwind)
+mod:RegisterCombat("scenario", 2212, 2828)
 
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START 297822 297746 304976 297574 304251 306726 299110 307863 300351 300388 304101 304282 306001 306199 303589 305875 306828 306617 300388 296537 305378 298630 298033 305236 304169 298502 297315",
-	"SPELL_AURA_APPLIED 311390 315385 316481 311641 299055",
+	"SPELL_AURA_APPLIED 311390 315385 316481 311641 299055 304165",
 	"SPELL_AURA_APPLIED_DOSE 311390",
 	"SPELL_AURA_REMOVED 298033",
 	"SPELL_CAST_SUCCESS 297237 305378",
@@ -80,19 +80,19 @@ local specWarnVisceralFluid			= mod:NewSpecialWarningDodge(305875, nil, nil, nil
 local specWarnToxicVolley			= mod:NewSpecialWarningDodge(304169, nil, nil, nil, 2, 2)
 
 --General
-local timerGiftoftheTitan		= mod:NewBuffFadesTimer(20, 313698, nil, nil, nil, 5)
+local timerGiftoftheTitan			= mod:NewBuffFadesTimer(20, 313698, nil, nil, nil, 5)
 --Affixes/Masks
-local timerDarkImaginationCD	= mod:NewCDTimer(60, 315976, nil, nil, nil, 1, 296733)
+local timerDarkImaginationCD		= mod:NewCDTimer(60, 315976, nil, nil, nil, 1, 296733)
 --Thrall
-local timerSurgingDarknessCD	= mod:NewCDTimer(20.6, 297822, nil, nil, nil, 3)
-local timerSeismicSlamCD		= mod:NewCDTimer(12.1, 297746, nil, nil, nil, 3)
+local timerSurgingDarknessCD		= mod:NewCDTimer(20.6, 297822, nil, nil, nil, 3)
+local timerSeismicSlamCD			= mod:NewCDTimer(12.1, 297746, nil, nil, nil, 3)
 --Extra Abilities (used by Thrall and the area LTs)
-local timerDefiledGroundCD		= mod:NewCDTimer(12.1, 306726, nil, nil, nil, 3)
+local timerDefiledGroundCD			= mod:NewCDTimer(12.1, 306726, nil, nil, nil, 3)
 --Other notable elite timers
-local timerSurgingFistCD		= mod:NewCDTimer(9.7, 300351, nil, nil, nil, 3)
-local timerDecimatorCD			= mod:NewCDTimer(9.7, 300412, nil, nil, nil, 3)
-local timerToxicBreathCD		= mod:NewCDTimer(7.3, 298502, nil, nil, nil, 3)
-local timerToxicVolleyCD		= mod:NewCDTimer(7.3, 304169, nil, nil, nil, 3)
+local timerSurgingFistCD			= mod:NewCDTimer(9.7, 300351, nil, nil, nil, 3)
+local timerDecimatorCD				= mod:NewCDTimer(9.7, 300412, nil, nil, nil, 3)
+local timerToxicBreathCD			= mod:NewCDTimer(7.3, 298502, nil, nil, nil, 3)
+local timerToxicVolleyCD			= mod:NewCDTimer(7.3, 304169, nil, nil, nil, 3)
 
 mod:AddInfoFrameOption(307831, true)
 mod:AddNamePlateOption("NPAuraOnHaunting2", 306545, false)
@@ -178,7 +178,7 @@ function mod:OnCombatStart(delay)
 		DBM:FireEvent("BossMod_EnableHostileNameplates")
 	end
 	if self.Options.InfoFrame then
-		DBM.InfoFrame:SetHeader(DBM:GetSpellInfo(307831))
+		DBM.InfoFrame:SetHeader(DBM:GetSpellName(307831))
 		DBM.InfoFrame:Show(5, "playerpower", 1, ALTERNATE_POWER_INDEX, nil, nil, 2)--Sorting lowest to highest
 	end
 end
@@ -228,13 +228,6 @@ function mod:SPELL_CAST_START(args)
 		timerDefiledGroundCD:Start()
 		if GetNumGroupMembers() > 1 then
 			self:BossTargetScanner(args.sourceGUID, "DefiledGroundTarget", 0.1, 7)
-		end
-	elseif spellId == 299055 then
-		if args:IsPlayer() then
-			specWarnDarkForce:Show()
-			specWarnDarkForce:Play("targetyou")
-		else
-			warnDarkForce:Show(args.destName)
 		end
 	elseif spellId == 299110 and self:AntiSpam(2, 2) then
 		specWarnOrbofAnnihilation:Show()
@@ -365,9 +358,16 @@ function mod:SPELL_AURA_APPLIED(args)
 			if GetNumGroupMembers() > 1 then
 				yellDesperateRetching:Yell()
 			end
-		elseif self:CheckDispelFilter() then
+		elseif self:CheckDispelFilter("disease") then
 			specWarnDesperateRetchingD:Show(args.destName)
 			specWarnDesperateRetchingD:Play("helpdispel")
+		end
+	elseif spellId == 299055 then
+		if args:IsPlayer() then
+			specWarnDarkForce:Show()
+			specWarnDarkForce:Play("targetyou")
+		else
+			warnDarkForce:Show(args.destName)
 		end
 	end
 end
@@ -404,26 +404,26 @@ end
 
 function mod:UNIT_DIED(args)
 	local cid = self:GetCIDFromGUID(args.destGUID)
-	if cid == 152089 then--Thrall
+	if cid == 152089 or cid == 234034 then--Thrall
 		timerSurgingDarknessCD:Stop()
 		timerSeismicSlamCD:Stop()
 		--timerCriesoftheVoidCD:Stop()
 		timerDefiledGroundCD:Stop()
 		DBM:EndCombat(self)
-	elseif cid == 156161 then--Inquisitor Gnshal
+	elseif cid == 156161 or cid == 234035 then--Inquisitor Gnshal
 		--timerCriesoftheVoidCD:Stop()
 		self.vb.GnshalCleared = true
-	elseif cid == 152874 then--Vez'okk the Lightless
+	elseif cid == 152874 or cid == 234037 then--Vez'okk the Lightless
 		timerDefiledGroundCD:Stop()
 		self.vb.VezokkCleared = true
-	elseif cid == 153943 then
+	elseif cid == 153943 then--Decimator Shiq'voth (unknown TWW variant ID)
 		timerSurgingFistCD:Stop()
 		timerDecimatorCD:Stop()
-	elseif cid == 153401 then--K'thir Dominator
+	elseif cid == 153401 or cid == 244186 then--K'thir Dominator
 		if self.Options.NPAuraOnAbyss then
 			DBM.Nameplate:Hide(true, args.destGUID, 298033)
 		end
-	elseif cid == 153532 then--Big Bug Guy
+	elseif cid == 153532 then--Aqir Mindhunter (Unknown TWW variant ID)
 		timerToxicVolleyCD:Stop()
 		timerToxicBreathCD:Stop()
 	end
@@ -431,14 +431,14 @@ end
 
 function mod:ENCOUNTER_START(encounterID)
 	if not self:IsInCombat() then return end
-	if encounterID == 2332 then--Thrall
+	if encounterID == 2332 or encounterID == 3086 then--Thrall
 		timerSurgingDarknessCD:Start(11.1)
 		if self.vb.VezokkCleared then
 			timerDefiledGroundCD:Start(1)
 		else
 			timerSeismicSlamCD:Start(4.6)
 		end
-	elseif encounterID == 2373 then--Vezokk
+	elseif encounterID == 2373 or encounterID == 3089 then--Vezokk
 		timerDefiledGroundCD:Start(3.4)
 	end
 end
--- AddOns/DBM-Challenges/CHANGELOG.md
+++ AddOns/DBM-Challenges/CHANGELOG.md
@@ -1,24 +1,9 @@
-# <DBM> Challenges
+# <DBM Mod> Challenges
 
-## [r151](https://github.com/DeadlyBossMods/DBM-Challenges/tree/r151) (2022-10-25)
-[Full Changelog](https://github.com/DeadlyBossMods/DBM-Challenges/compare/r150...r151) [Previous Releases](https://github.com/DeadlyBossMods/DBM-Challenges/releases)
+## [r168](https://github.com/DeadlyBossMods/DBM-Challenges/tree/r168) (2025-04-22)
+[Full Changelog](https://github.com/DeadlyBossMods/DBM-Challenges/compare/r167...r168) [Previous Releases](https://github.com/DeadlyBossMods/DBM-Challenges/releases)
 
-- Bump TOC file  
-- Disable massive charge timer, it's not consistent  
-- Fixed a bug where Anthanos' abilities were on wrong boss (Azaruux)  
-    Added basic ability warnings to Azeruux  
-- Fix invalid spellId  
-- More attempted fixes, because god forbid this be easy and just have proper usable events.  
-- hi luacheck  
-- This seems to be working, hopefully without any caveats i'll find later, cause every other thing seemed to work too, til it didn't  
-- Disable bad code that just makes all pulls wins. i'll leave it in current state that all pulls are losses instead unless yuo're targetting boss.  
-- One day i'll get this working. just sucks I have to spend so much time on something blizzard could have done faster :\  
-- Try to speed up and improve victory detection  
-- Add some content to Azaruux, Kalisthene, and Splinterbark  
-- More accurate and faster wipe detection  
-- Apparently find and replace exact matches, can't find all exact matches  
-- Fix numpty  
-- Attempt to imrpove wipe detection in PoA  
-- Fix all PoA mods incorrectly behaving like scenario mods  
-- Fix double consume warning  
-- remove the last timer from torghast mods.  
+- apparently i never remembered to bump tocs  
+- populate TWW variant creature and encounter IDs  
+- Enable visions loading in visions revisited  
+- Because DBT can't get it's shit together, all 77 (and counting) mods that do this just need to work around core bug  
