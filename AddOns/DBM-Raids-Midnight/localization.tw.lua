95876bf8ce246beab197a979c6cb99c2cf261822
Mili <milichang0410@gmail.com>
2026-04-14 18:46:32 +0800

    update: DBM

---
+++ AddOns/DBM-Raids-Midnight/localization.tw.lua
@@ -0,0 +1,31 @@
+if GetLocale() ~= "zhTW" then return end
+local L
+
+---------------------------
+--  LightblindedVanguard (3180) --
+---------------------------
+L= DBM:GetModLocalization(2737)
+
+L:SetMiscLocalization({
+	JudgementShield	= "審判-盾)",
+	JudgementFV		= "Judgment-FV)"
+})
+
+---------------------------
+--  Beloren, Child of Alar (3182) --
+---------------------------
+L= DBM:GetModLocalization(2739)
+
+L:SetMiscLocalization({
+	ColorSwap		= "顏色交換"
+})
+
+---------------------------
+--  Midnight Falls (3183) --
+---------------------------
+
+L= DBM:GetModLocalization(2740)
+
+L:SetMiscLocalization({
+	MemoryGame		= "記憶遊戲"
+})
