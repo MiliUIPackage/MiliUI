f2e11a87f37d0fe205d3be3266707d92f5251b3a
Mili <milichang0410@gmail.com>
2026-06-18 04:28:28 +0800

    remove: DBM

--- AddOns/DBM-Delves-Midnight/Zones/TheShadowEnclave.lua
+++
@@ -1,9 +0,0 @@
-local mod	= DBM:NewMod("z2952", "DBM-Delves-Midnight", 3)
---local L		= mod:GetLocalizedStrings()
-
-mod:SetRevision("20260515230945")
-mod:SetHotfixNoticeRev(20250220000000)
-mod:SetMinSyncRevision(20250220000000)
-mod:SetZone(2952)
-
-mod:RegisterCombat("scenario", 2952)
